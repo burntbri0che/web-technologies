4252e4ed6abee3c405785e0ac013d9f23084eb42
burntbri0che <111907700+burntbri0che@users.noreply.github.com>
2024-05-06 09:22:38 +0500

    set up server and routes

---
+++ Project/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("landing-page");
+});
+
+router.get("/contact-form", (req, res) => {
+    res.render("contact-form");
+});
+
+router.get("/ajax", (req, res) => {
+    res.render("ajax");
+});
+
+module.exports = router;
--- Project/server.js
+++ Project/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-let server = express();
-server.set("view engine","ejs");
-server.use(express.static("public"));
-server.get("/api", function(req,res){
-    res.render("api");
-})
-server.get("/", function(req,res){
-    res.render("design");
-})
+const server = express();
+
+const router = require("./routes/index.js");
 
 server.listen(3000);
+
+server.use(express.static("public"));
+server.set("view engine", "ejs");
+server.use("/", router);
